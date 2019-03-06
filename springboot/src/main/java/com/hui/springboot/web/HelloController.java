dbdd4e0c8c9fc42e2ce7e03513be3dd95d64454c
chunhb <@travelsky.com>
2019-03-06 15:24:10 +0800

    初始化一个springboot版本

---
+++ springboot/src/main/java/com/hui/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.hui.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/hellotest")
+    public String hellotest(){
+        return "hello word springboot";
+    }
+}
