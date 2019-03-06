725f0c88f3499b1201f1beee085a64e715bc081c
chunhb <@travelsky.com>
2019-03-06 17:32:21 +0800

    对jsp的支持修改

--- springboot/src/main/java/com/hui/springboot/web/HelloController.java
+++ springboot/src/main/java/com/hui/springboot/web/HelloController.java
@@ -1,13 +1,25 @@
 package com.hui.springboot.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
 public class HelloController {
 
     @RequestMapping(value="/hellotest")
     public String hellotest(){
         return "hello word springboot2";
+    }
+
+
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
     }
 }
