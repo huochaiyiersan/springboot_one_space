53f05014a14a73e4ba1ada24848a8e825800263c
chunhb <@travelsky.com>
2019-03-08 15:37:13 +0800

    页面遍历显示数据

--- springboot/src/main/java/com/hui/springboot/web/ProductController.java
+++ springboot/src/main/java/com/hui/springboot/web/ProductController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ProductController {
 
@@ -14,6 +17,17 @@ public class ProductController {
         Product currentProduct =new Product(5,"product e", 200);
 
         boolean testBoolean = false;
+
+        List<Product> ps = new ArrayList<>();
+        ps.add(new Product(1,"product a", 50));
+        ps.add(new Product(2,"product b", 100));
+        ps.add(new Product(3,"product c", 150));
+        ps.add(new Product(4,"product d", 200));
+        ps.add(currentProduct);
+        ps.add(new Product(6,"product f", 200));
+        ps.add(new Product(7,"product g", 200));
+
+        model.addAttribute("ps", ps);
 
         model.addAttribute("htmlContent", htmlContent);
         model.addAttribute("currentProduct", currentProduct);
